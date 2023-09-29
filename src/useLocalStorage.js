4ddf6065e31a82732f5facd3443c13e38d5826b5
DeikuModder <gabrieltc555@gmail.com>
2023-09-28 20:53:48 -0400

    adedd comments on useLocalStorage function

--- src/useLocalStorage.js
+++ src/useLocalStorage.js
@@ -1,24 +1,31 @@
 import { useState } from 'react'
 
+//A custom hook used for storage the values of the user in the browser's local storage. It take a key, that will hold the value, and an initial value that the key will store
+
 export const useLocalStorage = (key, initialValue) => {
+//we use the useState hook, and make an stored value, and a set new value function
   const [storedValue, setNewValue] = useState(() => {
+
+    //using a try catch in case of errors
     try {
-        const item = window.localStorage.getItem(key);
-        return item ? JSON.parse(item) : initialValue
+        const item = window.localStorage.getItem(key); //we get the item stored in the local storage within the key value that we passed
+        return item ? JSON.parse(item) : initialValue //if there is a key with a value there, we empty the key so we can store a new value in there, otherwise we return that key will hold the initial value
     } catch (error) {   
-        return initialValue
+        return initialValue //in case of an error, key will hold the initial value
     }
   });
 
   const setValue = value => {
+    //this function will set the value that the user passes in the key
+    //then again with a try cath in case of an error
     try {
-        setNewValue(value)
-        window.localStorage.setItem(key, JSON.stringify(value))
+        setNewValue(value) //with the setnew value function we made earlier, we set the the value that was passed in the function
+        window.localStorage.setItem(key, JSON.stringify(value)) //then set the key new value in the local storage, passing it trough the stringify, cause it must be an string so the local storage con hold it
     } catch (error) {
-        console.log(error)
+        console.error(error) //in case of an error return the error
     }
   }
 
-  return [storedValue, setValue];
+  return [storedValue, setValue]; //return an array that holds, the stored initial value, and the function to update it
 }
 
